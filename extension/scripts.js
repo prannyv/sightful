a661f6cdbd82cc0ac6eb089663e96ee187f332c1
bryant <bryant.zhg@gmail.com>
2023-08-19 13:58:45 -0400

    tab functionality working and checkbox/textfield

---
+++ extension/scripts.js
@@ -0,0 +1,82 @@
+// checkboxes
+const checkbox1 = document.getElementById('checkbox1');
+const checkbox2 = document.getElementById('checkbox2');
+const checkbox3 = document.getElementById('checkbox3');
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedCheckbox1State = localStorage.getItem('checkbox1');
+    const savedCheckbox2State = localStorage.getItem('checkbox2');
+    const savedCheckbox3State = localStorage.getItem('checkbox3');
+
+    if (savedCheckbox1State) {
+        checkbox1.checked = JSON.parse(savedCheckbox1State);
+    }
+
+    if (savedCheckbox2State) {
+        checkbox2.checked = JSON.parse(savedCheckbox2State);
+    }
+
+    if (savedCheckbox3State) {
+        checkbox3.checked = JSON.parse(savedCheckbox3State);
+    }
+});
+
+checkbox1.addEventListener('change', () => {
+    localStorage.setItem('checkbox1', checkbox1.checked);
+});
+
+checkbox2.addEventListener('change', () => {
+    localStorage.setItem('checkbox2', checkbox2.checked);
+});
+
+
+checkbox3.addEventListener('change', () => {
+    localStorage.setItem('checkbox3', checkbox3.checked);
+});
+
+// textfield
+// const textInput = document.getElementById('text-input');
+// const saveButton = document.getElementById('save-button');
+// const savedText = document.getElementById('saved-text');
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const savedTextValue = localStorage.getItem('savedText');
+
+//     if (savedTextValue) {
+//         savedText.textContent = `Saved Text: ${savedTextValue}`;
+//         console.log('Saved Text:', savedTextValue);
+//     }
+// });
+
+// saveButton.addEventListener('click', () => {
+//     const enteredText = textInput.value;
+//     localStorage.setItem('savedText', enteredText);
+//     savedText.textContent = `Saved Text: ${enteredText}`;
+//     console.log('Saved Text:', enteredText);
+// });
+
+
+const textInput = document.getElementById('text-input');
+const appendButton = document.getElementById('append-button');
+const appendedText = document.getElementById('appended-text');
+
+let appendedString = ''; // Initialize an empty string to store appended text
+
+appendButton.addEventListener('click', () => {
+    const enteredText = textInput.value.trim();
+
+    if (enteredText !== '') {
+        // Append entered text to the string and add a comma if needed
+        if (appendedString !== '') {
+            appendedString += ', ';
+        }
+        appendedString += enteredText;
+
+        // Update the displayed appended text
+        appendedText.textContent = `Appended Text: ${appendedString}`;
+
+        console.log('Appended String:', appendedString); // Print in console
+
+        textInput.value = ''; // Clear the input field
+    }
+});
